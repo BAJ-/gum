9efbdf4dacb11fa5d009dfdd922e4df6ad69f712
Bjørn A. Johansen <baj-@outlook.com>
2025-04-13 12:27:18 +0100

    use expand home path in install method

--- internal/version/version.go
+++ internal/version/version.go
@@ -3,11 +3,25 @@ package version
 import (
 	"fmt"
 	"io"
+	"os"
+	"path/filepath"
+)
+
+const (
+	InstallDir = "${HOME}/.gum/versions"
 )
 
 func Install(v string, w io.Writer) error {
 
 	v = normaliseVersion(v)
+
+	installDir := expandPath(InstallDir)
+	versionDir := filepath.Join(installDir, v)
+
+	if _, err := os.Stat(versionDir); err == nil {
+		fmt.Fprintf(w, "Go %s is already installed at %s\n", v, versionDir)
+		return nil
+	}
 
 	fmt.Fprintf(w, "Installing Go version %s\n", v)
 	return nil
